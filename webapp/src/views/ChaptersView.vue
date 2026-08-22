3d9f02369d9e834ee7933e6bc6e2fedc540d267b
Sarmad <auroras@pop-os.localdomain>
2026-08-22 15:04:56 -0400

    chapters: replace hero with door-knocking image

--- webapp/src/views/ChaptersView.vue
+++ webapp/src/views/ChaptersView.vue
@@ -1,7 +1,7 @@
 <template>
   <DefaultLayout>
     <template #header>
-      <Hero imageUrl="/city-council.jpg" :title="t('chapters.title')" :description="t('chapters.subtitle')" />
+      <Hero imageUrl="/door-knock.jpg" :title="t('chapters.title')" :description="t('chapters.subtitle')" />
     </template>
     <v-container>
       <v-row><v-col cols="12" md="10" lg="8" class="mx-auto">
